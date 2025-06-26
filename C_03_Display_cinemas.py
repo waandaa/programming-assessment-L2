e3d9065f6e3e1a6b31aabe5fa955cbd023bc0042
waandaa <wandahopkinson@mmc.school.nz>
2025-06-26 12:46:15 +1200

    Second trial display cinemas

--- C_03_Display_cinemas.py
+++ C_03_Display_cinemas.py
@@ -5,15 +5,28 @@ all_cinemas = [" Event Tauranga Crossing", "Event Tauranga Central", "Luxe Taura
 
 mini_cinema_dict = {
     'Cinemas': all_cinemas
-
-
 }
 
-# create dataframe / table from dictionary
-mini_cinema_frame = pandas.DataFrame(mini_cinema_dict)
+# Importing pandas package
+import pandas as pd
 
+# Importing numpy package
+import numpy as np
 
-print(mini_cinema_frame)
+# Creating a dictionary
+d = {
+    'Cinemas': all_cinemas
+}
+
+# Creating DataFrame
+df = pd.DataFrame(d)
+
+# Rearranging index
+df.index = np.arange(1, len(df) + 1)
+
+# Display modified DataFrame
+print("\n",df)
+
 print()
 
 cinema_selection = input("Type in your cinema selection ")
